aba06b4f104c4b8f27959c428064038dc8908024
Goodness Urama <goodyboy301@gmail.com>
2022-06-03 20:13:43 +0100

    style message

--- src/views/Chat/Message.vue
+++ src/views/Chat/Message.vue
@@ -3,7 +3,7 @@
     <textarea
       autofocus
       maxlength="160"
-      placeholder="Whats popping?"
+      placeholder="type a message..."
       spellcheck="false"
       rows="20"
     />
@@ -22,11 +22,18 @@ export default {};
   /* background: lightsalmon; */
   width: calc(100% / 3.1);
   display: flex;
-  align-items: center;
+  align-items: flex-end;
 }
 
 .send {
   width: 30px;
+  cursor: pointer;
+  border-radius: 100%;
+  margin: 2% 0;
+  transition: all 0.1s ease-in;
+}
+.send:hover {
+  transform: scale(1.1);
 }
 textarea,
 textarea:active {
@@ -38,22 +45,24 @@ textarea:active {
   color: black;
   resize: none;
   font-size: 16px;
-  line-height: 14.3px;
   height: min-content;
   display: flex;
-  height: 60px;
+  height: 30px;
   width: 90%;
   margin: 6px;
   padding: 4px;
   font-family: "Segoe UI";
   transition: all 0.2s ease-in;
+  border-radius: 30px;
 }
 textarea:focus {
   height: 80px;
   opacity: 1;
+  border-radius: 0;
 }
 textarea::placeholder {
-  opacity: 0.4;
-  color: #fff;
+  opacity: 0.8;
+  color: #444;
+  padding: 0 1ch;
 }
 </style>
