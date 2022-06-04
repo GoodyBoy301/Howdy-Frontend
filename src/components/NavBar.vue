01f970a25c978ccadd75fada70535b7cc6c3f137
Goodness Urama <goodyboy301@gmail.com>
2022-06-04 12:29:45 +0100

    Navbar elements

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <aside>
+      <span class="posts">
+        <i style="width: 50%" />
+        <i style="width: 40%" />
+        <i style="width: 50%" />
+      </span>
+      <span class="new"><Fab /></span>
+      <span class="contacts"><img src="../assets/Logo.svg" /></span>
+    </aside>
+  </div>
 </template>
 
 <script>
-export default {};
+import Fab from "./LayoutOne/Fab.vue";
+export default {
+  components: { Fab },
+};
 </script>
 
 <style scoped>
@@ -19,6 +32,34 @@ div {
     position: fixed;
     bottom: 0;
     display: block;
+  }
+  aside {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    width: clamp(1vw, 100vw, 300px);
+    margin: auto;
+    height: 100%;
+  }
+  span {
+    padding: 10px;
+    position: relative;
+    display: grid;
+  }
+  .new {
+    margin: 0 calc(50% - 33px);
+  }
+  .posts i {
+    width: 75%;
+    height: 50%;
+    /* padding:1px 10px; */
+    background: var(--primary);
+  }
+  .contacts {
+    display: flex;
+  }
+  .contacts img {
+    width: 60%;
+    margin-left: auto;
   }
 }
 </style>
