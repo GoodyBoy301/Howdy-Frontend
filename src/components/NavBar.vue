ebdaae758e0fa8f40491ddb7a1c64d5e881ecf23
Goodness Urama <goodyboy301@gmail.com>
2022-06-04 10:52:55 +0100

    Add Navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  background: black;
+  height: 50px;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+}
+</style>
