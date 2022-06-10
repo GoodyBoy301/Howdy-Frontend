c660a80d84c2fad1b415127633a3f1cd87651fbb
Goodness Urama <goodyboy301@gmail.com>
2022-06-10 23:45:49 +0100

    add contacts header

--- src/Components/ContactsNav.vue
+++ src/Components/ContactsNav.vue
@@ -5,6 +5,11 @@
         style="--color: #24c840"
       />
     </div>
+    <div class="heading">
+      <h2>Contacts</h2>
+      <img src="/icons/search.svg" alt="" />
+      <img src="/icons/plus.svg" alt="" />
+    </div>
   </aside>
 </template>
 
@@ -19,18 +24,38 @@ aside {
   width: 100%;
   height: 100%;
 }
+div {
+  margin: 5%;
+}
 .mac {
   display: flex;
-  align-items: center;
-  height: 9%;
-  margin-left: 5%;
+  align-items: flex-end;
+  height: 4%;
 }
 .mac span {
   width: 12px;
   height: 12px;
   margin: 1%;
-  border-radius: 100%;
+  border-radius: 50%;
   display: block;
   background: var(--color);
+}
+.heading {
+  display: flex;
+}
+h2 {
+  font-weight: 500;
+}
+.heading img {
+  width: 2em;
+  height: 2em;
+  padding: 0.6em;
+  background: var(--lightPrimary);
+  border-radius: 50%;
+  fill: white;
+  stroke: black;
+}
+.heading img:first-of-type {
+  margin: 0 0.55em 0 auto;
 }
 </style>
