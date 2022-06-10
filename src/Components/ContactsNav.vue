73eaa5dd9125c234772eb4e9cc00d42c281d89ca
Goodness Urama <goodyboy301@gmail.com>
2022-06-10 23:26:24 +0100

    Add mac dots

--- src/Components/ContactsNav.vue
+++ src/Components/ContactsNav.vue
@@ -1,5 +1,11 @@
 <template>
-  <aside />
+  <aside>
+    <div class="mac">
+      <span style="--color: #fe5f58" /><span style="--color: #ffbc2e" /><span
+        style="--color: #24c840"
+      />
+    </div>
+  </aside>
 </template>
 
 <script>
@@ -9,8 +15,22 @@ export default {};
 <style>
 aside {
   background: var(--darkPrimary);
-  border-radius: 25px 0 0 25px;
+  border-radius: 15px 0 0 15px;
   width: 100%;
   height: 100%;
+}
+.mac {
+  display: flex;
+  align-items: center;
+  height: 9%;
+  margin-left: 5%;
+}
+.mac span {
+  width: 12px;
+  height: 12px;
+  margin: 1%;
+  border-radius: 100%;
+  display: block;
+  background: var(--color);
 }
 </style>
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -9,7 +9,7 @@ export default {};
 <style>
 .chat {
   background: var(--lightPrimary);
-  border-radius: 0 25px 25px 0;
+  border-radius: 0 15px 15px 0;
   width: 100%;
   height: 100%;
 }
