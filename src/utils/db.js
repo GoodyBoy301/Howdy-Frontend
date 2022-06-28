5c98cade0d9de4cc1058ca5e96e96e44a362e7cd
Goodness Urama <goodyboy301@gmail.com>
2022-06-28 10:06:19 +0100

    add fetchLoggedIn to window.db

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,3 +3,6 @@ window.db = {};
 window.db.clear = clear;
 window.db.set = set;
 window.db.get = get;
+
+import fetchLoggedIn from "../modules/fetchLoggedIn.js";
+window.db.fetchLoggedIn = fetchLoggedIn;
