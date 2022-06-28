de49337ee4ef3623767adf0a6b71101e5b42eb74
Goodness Urama <goodyboy301@gmail.com>
2022-06-28 15:53:20 +0100

    show contactsnav on mobile

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,29 @@
-<template></template>
+<template>
+  <section class="chat home">
+    <div class="heading bar">
+      <li class="topbar">
+        <img src="/assets/icons/back.svg" alt="" class="mobile back" />
+        <img src="/assets/DPs/male02.png" alt="" class="dp" />
+        <div>
+          <h3>Kush Gibson</h3>
+          <p>Online 8 mins ago</p>
+        </div>
+        <div><img src="/assets/icons/dots.svg" alt="" /></div>
+      </li>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+/* Mobile Devices*/
+@media (max-width: 576px) {
+  .home {
+    grid-area: 1;
+    width: 0;
+  }
+}
+</style>
