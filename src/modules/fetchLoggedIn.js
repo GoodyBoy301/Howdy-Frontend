90d71f6b5726270eb3d55b44bab0c79f7cb22c34
Goodness Urama <goodyboy301@gmail.com>
2022-06-28 10:06:39 +0100

    edit fetchLoggedIn

--- src/modules/fetchLoggedIn.js
+++ src/modules/fetchLoggedIn.js
@@ -1,9 +1,16 @@
 import { get } from "idb-keyval";
 
 export default async function fetchLoggedIn() {
+  let bool;
   await get("user")
     .then((x) => {
-      if (x) return true;
+      if (x) {
+        bool = true;
+      } else {
+        bool = false;
+      }
     })
     .catch((err) => console.log(err));
+  return bool;
 }
+
