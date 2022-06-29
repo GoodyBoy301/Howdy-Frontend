9f95c95117f4426d12661727e74d78269d829d53
Goodness Urama <goodyboy301@gmail.com>
2022-06-29 21:33:53 +0100

    Edit home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="chat home">
-    <div class="heading bar">
+    <!-- <div class="heading bar">
       <li class="topbar">
         <img src="/assets/icons/back.svg" alt="" class="mobile back" />
         <img src="/assets/DPs/male02.png" alt="" class="dp" />
@@ -10,7 +10,24 @@
         </div>
         <div><img src="/assets/icons/dots.svg" alt="" /></div>
       </li>
-    </div>
+    </div> -->
+
+    <section class="profile">
+      <div class="banner">
+        <img
+          :src="`/assets/DPs/${pic || 'male04'}.png`"
+          alt=""
+          class="previewDP profileDP"
+        />
+      </div>
+      <div>
+        <h5 class="myName">Goodness Urama</h5>
+        <h6 class="bio">
+          QQ sse aeseq ESRW SFCRDD ESEA EDASA AESEA AEDSXESDB SRFDAR SRSXE
+          srdrsfds srfdxrds ssrfdsrdrssb srf
+        </h6>
+      </div>
+    </section>
     <h3 class="logout__desktop" @click="Logout">Logout</h3>
   </section>
 </template>
@@ -32,7 +49,28 @@ export default {
 .home {
   position: relative;
 }
-
+.profile {
+  display: grid;
+  background: white;
+  height: 100%;
+  justify-content: flex-start;
+  grid-template-rows: 5fr 1fr 4fr;
+}
+.banner {
+  background: yellow;
+  height: 150px;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.previewDP.profileDP {
+  position: relative;
+  top: 60%;
+  background: inherit;
+  border: 3px solid white;
+  box-sizing: content-box;
+}
 .logout__desktop {
   position: absolute;
   bottom: 1em;
@@ -40,8 +78,24 @@ export default {
   right: 0;
   text-align: center;
   color: #fe5f58;
+  cursor: pointer;
 }
-
+.myName {
+  color: var(--darkPrimary);
+  text-align: center;
+  font-size: 1.5rem;
+  width: 75%;
+  margin: auto;
+  font-weight: 600;
+}
+.bio {
+  color: var(--darkPrimary);
+  text-align: center;
+  font-size: 1rem;
+  width: 75%;
+  margin: 3% auto;
+  font-weight: 500;
+}
 /* Mobile Devices*/
 @media (max-width: 576px) {
   .chat.home {
