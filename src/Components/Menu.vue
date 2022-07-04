bff723f8894029beabf7c1f0264692d928b0dca2
Goodness Urama <goodyboy301@gmail.com>
2022-07-04 18:16:36 +0100

    Create Menu Component

---
+++ src/Components/Menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div @mouseenter="handleMenu">
+    <img src="/assets/icons/dots.svg" alt="" @click="handleMenu" />
+    <div class="userInfo">
+      <section class="profile">
+        <div class="banner">
+          <img
+            :src="`/assets/DPs/${Contact.pic}.png`"
+            alt=""
+            class="previewDP profileDP"
+          />
+        </div>
+        <div>
+          <h5 class="myName">{{ Contact?.name }}</h5>
+          <h6 class="bio">
+            {{ Contact?.bio }}
+          </h6>
+        </div>
+      </section>
+      <h3 class="logout__desktop" @click="Logout">Logout</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["Contact"],
+  setup() {
+    const handleMenu = (e) => {
+      console.log(e.target);
+    };
+
+    return { handleMenu };
+  },
+};
+</script>
+
+<style></style>
