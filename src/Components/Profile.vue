aa16a865e977c8a0b8f91829d4c013455900d9dc
Goodness Urama <goodyboy301@gmail.com>
2022-07-05 07:37:50 +0100

    Create Profile component

---
+++ src/Components/Profile.vue
@@ -0,0 +1,94 @@
+<template>
+  <section class="profile" :style="`--dp:${User.color}`">
+    <div class="banner">
+      <img
+        :src="`/assets/DPs/${User.pic}.png`"
+        alt=""
+        class="previewDP profileDP"
+      />
+    </div>
+    <div>
+      <h5 class="myName">{{ User?.name }}</h5>
+      <h6 class="bio">
+        {{ User?.bio }}
+      </h6>
+    </div>
+  </section>
+  <h3 class="logout__desktop" @click="Logout">Logout</h3>
+</template>
+
+<script>
+export default {
+  props: ["User", "setLoggedIn"],
+  setup({ setLoggedIn }) {
+    const Logout = () => {
+      clear().then(() => setLoggedIn());
+    };
+    return { Logout };
+  },
+};
+</script>
+
+<style>
+.profile {
+  display: grid;
+  background: var(--lightPrimary);
+  height: 100%;
+  justify-content: flex-start;
+  grid-template-rows: 5fr 1fr 4fr;
+  grid-template-columns: 1fr;
+}
+
+.banner {
+  background: var(--dp);
+  height: 150px;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.previewDP.profileDP {
+  position: relative;
+  top: 60%;
+  background: inherit;
+  border: 3px solid var(--lightPrimary);
+  box-sizing: content-box;
+}
+.logout__desktop {
+  position: absolute;
+  bottom: 1em;
+  left: 0;
+  right: 0;
+  text-align: center;
+  color: #fe5f58;
+  cursor: pointer;
+}
+.myName {
+  color: white;
+  text-align: center;
+  font-size: 1.5rem;
+  width: 75%;
+  margin: auto;
+  font-weight: 600;
+}
+.bio {
+  color: white;
+  text-align: center;
+  font-size: 1rem;
+  width: 75%;
+  margin: 3% auto;
+  font-weight: 500;
+}
+/* Mobile Devices*/
+@media (max-width: 576px) {
+  .logout__desktop {
+    display: none;
+  }
+  .banner,
+  .bio,
+  .myName {
+    display: none;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,21 @@
 <template>
   <section class="chat home">
-    <!-- <div class="heading bar">
-      <li class="topbar">
-        <img src="/assets/icons/back.svg" alt="" class="mobile back" />
-        <img src="/assets/DPs/male02.png" alt="" class="dp" />
-        <div>
-          <h3>Kush Gibson</h3>
-          <p>Online 8 mins ago</p>
-        </div>
-        <div><img src="/assets/icons/dots.svg" alt="" /></div>
-      </li>
-    </div> -->
-
-    <section class="profile" :style="`--dp:${User.color}`">
-      <div class="banner">
-        <img
-          :src="`/assets/DPs/${User.pic}.png`"
-          alt=""
-          class="previewDP profileDP"
-        />
-      </div>
-      <div>
-        <h5 class="myName">{{ User?.name }}</h5>
-        <h6 class="bio">
-          {{ User?.bio }}
-        </h6>
-      </div>
-    </section>
-    <h3 class="logout__desktop" @click="Logout">Logout</h3>
+    <Profile :User="User" :setLoggedIn="setLoggedIn" />
   </section>
 </template>
 
 <script>
 import { ref } from "vue";
 import { clear, get } from "idb-keyval";
+import Profile from "../Components/Profile.vue";
 export default {
+  components: { Profile },
   props: ["setLoggedIn"],
   setup({ setLoggedIn }) {
     const User = ref({});
     get("user").then((user) => (User.value = user));
-    const Logout = () => {
-      clear().then(() => setLoggedIn());
-    };
 
-    return { Logout, User };
+    return { User };
   },
 };
 </script>
@@ -52,67 +24,12 @@ export default {
 .home {
   position: relative;
 }
-.profile {
-  display: grid;
-  background: var(--lightPrimary);
-  height: 100%;
-  justify-content: flex-start;
-  grid-template-rows: 5fr 1fr 4fr;
-  grid-template-columns: 1fr;
-}
-.banner {
-  background: var(--dp);
-  height: 150px;
-  margin: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.previewDP.profileDP {
-  position: relative;
-  top: 60%;
-  background: inherit;
-  border: 3px solid var(--lightPrimary);
-  box-sizing: content-box;
-}
-.logout__desktop {
-  position: absolute;
-  bottom: 1em;
-  left: 0;
-  right: 0;
-  text-align: center;
-  color: #fe5f58;
-  cursor: pointer;
-}
-.myName {
-  color: white;
-  text-align: center;
-  font-size: 1.5rem;
-  width: 75%;
-  margin: auto;
-  font-weight: 600;
-}
-.bio {
-  color: white;
-  text-align: center;
-  font-size: 1rem;
-  width: 75%;
-  margin: 3% auto;
-  font-weight: 500;
-}
+
 /* Mobile Devices*/
 @media (max-width: 576px) {
   .chat.home {
     grid-area: 1;
     width: 0;
-  }
-  .logout__desktop {
-    display: none;
-  }
-  .banner,
-  .bio,
-  .myName {
-    display: none;
   }
 }
 </style>
